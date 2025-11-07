28bc23db8b9e64a975405417e2dfa7873da1be74
JavidPack <javidpack@gmail.com>
2025-11-07 01:37:48 -0700

    v1.9.3

--- HelpfulHotkeys.cs
+++ HelpfulHotkeys.cs
@@ -56,7 +56,7 @@ namespace HelpfulHotkeys
 			SmartQuickStackToChestsHotKey = KeybindLoader.RegisterKeybind(this, "SmartQuickStackToChests", "OemPipe");
 			QuickUseItemHotkeys = new ModKeybind[10];
 			for (int i = 0; i < 10; i++) {
-				if (!HelpfulHotkeysClientConfig.Instance.EnableQuickUseItems11to19 && i != 9)	
+				if (!HelpfulHotkeysClientConfig.Instance.EnableQuickUseItems11to19 && i != 9)
 					continue;
 				QuickUseItemHotkeys[i] = KeybindLoader.RegisterKeybind(this, $"QuickUseItem{i + 11}", i == 9 ? "L" : "Z");
 			}
